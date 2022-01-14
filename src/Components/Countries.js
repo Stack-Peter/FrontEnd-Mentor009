76381cef9439279c59691da6142bbf96bd8e06bd
Peter Oliveira <stack.peter21@gmail.com>
2022-01-14 20:07:50 -0300

    Att 1

--- src/Components/Countries.js
+++ src/Components/Countries.js
@@ -13,11 +13,11 @@ const Countries = () => {
             {data && data.map((i, index) => (
                 <div key={index} className='Country'>
                     <img src={i.flags.png} />
-                    <p>{i.name}</p>
+                    <h3>{i.name}</h3>
                     <ul>
-                        <li>{i.population}</li>
-                        <li>{i.region}</li>
-                        <li>{i.capital}</li>
+                        <li><strong>Population: </strong>{i.population}</li>
+                        <li><strong>Region: </strong>{i.region}</li>
+                        <li><strong>Capital: </strong>{i.capital}</li>
                     </ul>
                 </div>
             ))}
